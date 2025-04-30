f9bfdaa0e16e72e9f2865741175102618e80580d
Steffen70 <steffen@seventy.mx>
2025-04-30 17:25:24 +0200

    Added a favicon and removed source map links to eliminate 404 errors in the server log.

--- frontend/build.sh
+++ frontend/build.sh
@@ -5,4 +5,7 @@ rm -rf ./Publish
 
 # Publish the project to the output directory
 export API_URL="https://localhost:8444/"
-dotnet publish -c Release -o ./Publish -v:d -p:ApiUrl=$API_URL
+dotnet publish -c Release -o ./Publish -v:d -p:ApiUrl=$API_URL -p:EmitSourceMapping=true -p:EmitDebugInformation=true
+
+# Strip sourceMappingURL comment to prevent browser from requesting .js.map
+find ./Publish -type f -name "*.js" -exec sed -i '/sourceMappingURL/d' {} +
